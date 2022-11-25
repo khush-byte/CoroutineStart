e19497a5bad660dbe2a352950eb08d5451d0b547
khush-byte <khush4450@gmail.com>
2022-11-25 15:53:38 +0500

    the same function without coroutines

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.coroutinestart
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.lifecycle.lifecycleScope
 import com.example.coroutinestart.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -20,9 +20,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
             //Use coroutine, prevent memory leak
-            lifecycleScope.launch(){
-                loadData()
-            }
+//            lifecycleScope.launch() {
+//                //Running the suspend function
+//                loadData()
+//            }
+            loadWithoutCoroutine()
         }
     }
 
@@ -31,18 +33,73 @@ class MainActivity : AppCompatActivity() {
         Log.d("MyLog", "Load started: $this")
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
-
         val city = loadCity()
+
         binding.tvCity.text = city
         val temp = loadTemperature(city)
+
         binding.tvTemp.text = temp.toString()
         binding.progress.isVisible = false
         binding.buttonLoad.isEnabled = true
         Log.d("MyLog", "Load finished: $this")
     }
 
+    private fun loadWithoutCoroutine(step: Int = 0, obj: Any? = null) {
+        when (step) {
+            0 -> {
+                Log.d("MyLog", "Load started: $this")
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                loadCityWithoutCoroutine {
+                    loadWithoutCoroutine(1, it)
+                }
+            }
+            1 -> {
+                val city = obj as String
+                binding.tvCity.text = city
+                loadTemperatureWithoutCoroutine(city) {
+                    loadWithoutCoroutine(2, it)
+                }
+            }
+            2 -> {
+                val temp = obj as Int
+                binding.tvTemp.text = temp.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+                Log.d("MyLog", "Load finished: $this")
+            }
+        }
+    }
+
+    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
+//        thread {
+//            Thread.sleep(5000)
+//            runOnUiThread {
+//                callback.invoke("Moscow")
+//            }
+//        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke("Moscow")
+        }, 5000)
+    }
+
+
+    private fun loadTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+//        Thread.sleep(5000)
+//        runOnUiThread {
+//            callback.invoke(17)
+//        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback.invoke(17)
+        }, 5000)
+    }
+
     private suspend fun loadCity(): String {
-        //Thread.sleep(5000)
         delay(5000)
         return "Moscow"
     }
@@ -50,10 +107,9 @@ class MainActivity : AppCompatActivity() {
     private suspend fun loadTemperature(city: String): Int {
         Toast.makeText(
             this,
-            getString(R.string.loading_temperature_toast) + city,
+            getString(R.string.loading_temperature_toast, city),
             Toast.LENGTH_SHORT
         ).show()
-        //Thread.sleep(5000)
         delay(5000)
         return 17
     }
